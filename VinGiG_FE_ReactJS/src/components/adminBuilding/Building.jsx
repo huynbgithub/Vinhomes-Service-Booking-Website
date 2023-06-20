9c38d411411b8eaa4102804299ce26ae2f37ad19
ttdta <1000kgdat@gmail.com>
2023-06-20 16:55:31 +0700

    updatepart2

--- VinGiG_FE_ReactJS/src/components/adminBuilding/Building.jsx
+++ VinGiG_FE_ReactJS/src/components/adminBuilding/Building.jsx
@@ -6,48 +6,74 @@ function Building() {
 
   return (
     <>
-      <table>
-        <tbody><tr>
-          <td>
-            <form autoComplete="off" onsubmit="onFormSubmit()">
-              <div>
-                <label htmlFor="productCode">Product Code</label>
-                <input type="text" name="productCode" id="productCode" />
-              </div>
-              <div>
-                <label htmlFor="product">Product Name</label>
-                <input type="text" name="product" id="product" />
-              </div>
-              <div>
-                <label htmlFor="qty">Qty</label>
-                <input type="number" name="qty" id="qty" />
-              </div>
-              <div>
-                <label htmlFor="perPrice">Price</label>
-                <input type="number" name="perPrice" id="perPrice" />
-              </div>
-              <div className="form_action--button">
-                <input type="submit" defaultValue="Submit" />
-                <input type="reset" defaultValue="Reset" />
-              </div>
-            </form>
-          </td><td>
-            <table className="list" id="storeList">
-              <thead>
-                <tr>
-                  <th>Product Code</th>
-                  <th>Product Name</th>
-                  <th>Qty</th>
-                  <th>Price</th>
-                </tr>
-              </thead>
-              <tbody>
-              </tbody>
-            </table>
-          </td>
-        </tr>
-        </tbody></table>
+    <div>
+  <main>
+    <div className="container text-center">
+      <h1 className="bg-light py-4 text-info">
+        <i className="fas fa-plug" /> Electronic Store
+      </h1>
+      <div className="d-flex justify-content-center">
+        <form className="w-50">
+          <input type="text" id="userid" className="form-control" autoComplete="off" readOnly placeholder="ID" />
+          <input type="text" id="proname" className="form-control" autoComplete="off" placeholder="Product Name" />
+          <div className="row">
+            <div className="col">
+              <input type="text" id="seller" className="form-control m-0" autoComplete="off" placeholder="Seller" />
+            </div>
+            <div className="col">
+              <input type="text" id="price" className="form-control m-0" autoComplete="off" placeholder="Price" />
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className="d-flex justify-content-center">
+        <button className="btn btn-success" id="btn-create">Create</button>
+        <button className="btn btn-primary" id="btn-read">Read</button>
+        <button className="btn btn-warning" id="btn-update">Update</button>
+        <button className="btn btn-danger" id="btn-delete">Delete All</button>
+      </div>
+      {/*  Table */}
+      <div className="d-flex table-data">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Product Name</th>
+              <th scope="col">Seller</th>
+              <th scope="col">Price</th>
+              <th scope="col">Edit</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody id="tbody">
+            {/*
+                  <tr>
+                      <th scope="row">1</th>
+                      <td>Mark</td>
+                      <td>Otto</td>
+                      <td>@mdo</td>
 
+                  </tr>
+                  <tr>
+                      <th scope="row">2</th>
+                      <td>Jacob</td>
+                      <td>Thornton</td>
+                      <td>@fat</td>
+                  </tr>
+                  */}
+          </tbody>
+        </table>
+      </div>
+      <div id="notfound" />
+    </div>
+  </main>
+  {/* Labels */}
+  <div className="w-100 btn btn-success insertmsg">Data Inserted Successfully...!</div>
+  <div className="w-100 btn btn-warning updatemsg">Data Updated..! Refresh Database To See Result</div>
+  <div className="w-100 btn btn-danger deletemsg">Data Deleted..!</div>
+  {/* Include Dexie */}
+  {/* Custom main file*/}
+</div>
 
     </>
   )
