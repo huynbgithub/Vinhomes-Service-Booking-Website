c797ecb0695e9e94a22f69f61c0e2d925748fbfd
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2023-06-22 09:11:37 +0700

    Customer Chat (completed)

--- VinGiG_FE_ReactJS/src/components/customerChat/ChatBox.jsx
+++ VinGiG_FE_ReactJS/src/components/customerChat/ChatBox.jsx
@@ -1,32 +1,30 @@
 import React, { useEffect, useState } from "react"
 import "./style.css"
-import { Link, useHistory } from "react-router-dom";
 import axios from 'axios';
 
 function ChatBox({ bookingID }) {
     const [content, setContent] = useState("");
 
     const [messages, setMessages] = useState([]);
-    const history = useHistory();
     useEffect(() => {
+        loadMessages();
+    }, [bookingID]);
+    function loadMessages() {
         axios.get(`http://localhost:8081/vingig/booking/${bookingID}/bookingMessage`)
             .then(res => {
                 const messages = res.data;
                 setMessages(messages);
             })
             .catch(error => console.log(error));
-    }, [bookingID]);
-
-    function send(event) {
+    }
+    async function send(event) {
         event.preventDefault();
-        axios.post(`http://localhost:8081/vingig/booking/${bookingID}/bookingMessage`,
+        await axios.post(`http://localhost:8081/vingig/booking/${bookingID}/bookingMessage`,
             {
                 content: content,
                 sendBy: true,
-                time: new Date().toLocaleString(),
-            }
-        ).then(history.push("/customer/chat"))
-            .catch(error => console.log(error));
+            }).catch(error => console.log(error));
+        loadMessages();
     }
     return (
         <div className="chat">
@@ -52,11 +50,29 @@ function ChatBox({ bookingID }) {
             <div className="chat-history">
                 <ul className="m-b-0">
                     {messages.map(message => {
+                        var epochTime = message.time;
+
+                        var dateObj = new Date(epochTime);
+
+                        var date = dateObj.getDate();
+                        var month = dateObj.getMonth() + 1;
+                        var year = dateObj.getYear() - 100 + 2000;
+                        var hours = dateObj.getHours();
+                        var minutes = dateObj.getMinutes();
+                        var seconds = dateObj.getSeconds();
+
+                        var formattedTime =
+                            date.toString() + '/' +
+                            month.toString() + '/' +
+                            year.toString() + ' ' +
+                            hours.toString().padStart(2, '0') + ':' +
+                            minutes.toString().padStart(2, '0') + ':' +
+                            seconds.toString().padStart(2, '0');
                         if (message.sendBy === false) {
                             return (
                                 <li className="clearfix">
                                     <div className="message-data">
-                                        <span className="message-data-time">{message.time}</span>
+                                        <span className="message-data-time">{formattedTime}</span>
                                     </div>
                                     <div className="message my-message">{message.content}</div>
                                 </li>
@@ -65,7 +81,8 @@ function ChatBox({ bookingID }) {
                             return (
                                 <li className="clearfix">
                                     <div className="message-data text-right">
-                                        <span className="message-data-time">{message.time}</span>
+                                        <span className="message-data-time">
+                                            {formattedTime}</span>
                                     </div>
                                     <div className="message other-message float-right">{message.content}</div>
                                 </li>
@@ -75,11 +92,11 @@ function ChatBox({ bookingID }) {
                 </ul>
             </div>
             <div className="chat-message clearfix">
-                <form className="input-group mb-0 inputContainer">
+                <form className="input-group mb-0 inputContainer" onSubmit={(event) => send(event)}>
                     <input type="text" className="form-control" value={content} placeholder="Enter text here..." onChange={(event) => { setContent(event.target.value); }} />
                     {/* <input type="hidden" className="form-control" value={bookingID} placeholder="Enter text here..." onChange={(event) => { setBookingID(event.target.value); }} /> */}
                     <div className="input-group-prepend" >
-                        <button type="submit" className="input-group-text"><i className="fa fa-send" onClick={(event) => send(event)} /></button>
+                        <button type="submit" className="input-group-text"><i className="fa fa-send" /></button>
                     </div>
                 </form>
             </div>
