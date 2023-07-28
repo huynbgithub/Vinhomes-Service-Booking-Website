32904ca8102c01f82a1863db1006a73089059bdb
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2023-07-28 12:21:33 +0700

    Firebase Key Encryption

--- VinGiG_FE_ReactJS/src/firebase.js
+++ VinGiG_FE_ReactJS/src/firebase.js
@@ -2,13 +2,9 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC7-EPzmPQ4xjeV2jeaQx0v0WmIRywiy9E",
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
     authDomain: "vingig-fpt.firebaseapp.com",
     projectId: "vingig-fpt",
     storageBucket: "vingig-fpt.appspot.com",
@@ -17,7 +13,6 @@ const firebaseConfig = {
     measurementId: "G-7Y5NR5HBB2"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const storage = getStorage(app);
